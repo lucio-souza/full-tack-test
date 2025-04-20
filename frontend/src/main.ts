e4aabf9cc68e045c2031792f7dc157f17439418a
lucio <desouzaleitelucioweslley@gmail.com>
2025-04-19 23:46:19 -0300

    consertando importação do main.ts e ajustando logo e titulo da aplicação

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './routers/indext'
+import router from './routers/index.ts'
 
 createApp(App).use(router).mount('#app')
