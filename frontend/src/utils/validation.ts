df8a1c08d245bba8add48376baec468d481be4b7
lucio <desouzaleitelucioweslley@gmail.com>
2025-04-20 04:12:53 -0300

    criando validações do formulario de login com o zod

---
+++ frontend/src/utils/validation.ts
@@ -0,0 +1,35 @@
+// utils/validation.ts
+import { z } from 'zod';
+
+export const validateLogin = (email: string, senha: string) => {  
+    const emailSchema = z.string()
+    .nonempty("Email é obrigatório")
+    .email('Formato de email inválido');
+    
+  const senhaSchema = z.string()
+    .nonempty("Senha é obrigatória")
+    .min(4, 'A senha deve ter pelo menos 6 caracteres');
+
+  let errors = {
+    emailError: '',
+    senhaError: ''
+  };
+
+  try {
+    emailSchema.parse(email);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      errors.emailError = err.errors[0].message;
+    }
+  }
+
+  try {
+    senhaSchema.parse(senha);
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      errors.senhaError = err.errors[0].message;
+    }
+  }
+
+  return errors;
+};
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,89 @@
+<script lang="ts">
+import axios from 'axios';
+import Input from '../components/Input.vue';
+import Button from '../components/Button.vue';
+
+export default {
+    name:'Login',
+    components: {
+    Input,
+    Button
+  },
+    data() {
+        return{
+            email:'',
+            senha:'',
+            mensagem:''
+        }   
+    },
+    methods:{
+        logar (){
+            //const dados={
+            //    email:this.email,
+            //    senha:this.senha
+            //}
+            axios.get('http://localhost:8080/user/')
+            .then(()=>{this.mensagem ='deu certo',console.log("deu certo");
+            })
+            .catch(error=>{console.log('deu errado',error);
+            })
+        }
+    }
+}
+</script>
+
+<template>
+    <main class="view">
+    <section class="left">
+        <img src="../assets/logo.png" alt="" class="logo">
+        <p class="text-login">Faça seu Login</p>
+        <form @submit.prevent="logar" class="form">
+            <Input type="text" placeholder="Email" v-model="email" />
+            <Input type="text" placeholder="Senha" v-model="senha" />
+            <Button type="submit" msg="Enviar"/>
+        </form>
+        <p>faça seu <a href="#" target="_blank" rel="noopener noreferrer">cadastro aqui</a> </p>
+    </section>
+    <section class="right">
+        <img src="../assets/view-tennis-racket-hitting-ball.png" alt="" id="img-view">
+    </section>
+    </main>
+</template>
+
+<style scoped>
+.view{
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+}
+a{
+    text-decoration: none;
+    color: #1a0dab;
+
+}
+.text-login{
+    font-size: 40px;
+    margin: 30px;
+}
+.view > section {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+.form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.logo{
+    margin-top: -50px;
+    margin-bottom: -50px;
+}
+#img-view {
+    
+  width: 100%;
+  height: 100%;
+}
+</style>
