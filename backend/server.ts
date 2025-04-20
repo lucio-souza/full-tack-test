4833ff525d90d07750c12b8a18a8ebc3c37674f7
lucio <desouzaleitelucioweslley@gmail.com>
2025-04-19 23:40:42 -0300

    adcionando configuração do cors para aceitar requisições de qualquer origem

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import courtsRouter from './src/routes/courtsRoutes';
-import userRouter from './src/routes/usersRoutes';
+  import express from 'express';
+  import dotenv from 'dotenv';
+  import courtsRouter from './src/routes/courtsRoutes';
+  import userRouter from './src/routes/usersRoutes';
+  import cors, { CorsOptions } from 'cors';
 
-dotenv.config();
+  dotenv.config();
+  
+  const corsOptions:CorsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
 
-const app = express();
-const PORT = process.env.PORT;
+  const app = express();
+  const PORT = process.env.PORT;
 
-app.use(express.json());
-app.use("/courts",courtsRouter);
-app.use("/user",userRouter);
+  app.use(express.json());
+  app.use(cors(corsOptions))
+  app.use("/courts",courtsRouter);
+  app.use("/user",userRouter);
 
-app.listen(PORT, () => {
-  console.log(`Servidor On 🔥🔥 na porta ${PORT}`);
-}
-);
+  app.listen(PORT, () => {
+    console.log(`Servidor On 🔥🔥 na porta ${PORT}`);
+  }
+  );
