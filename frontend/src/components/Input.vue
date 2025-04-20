455b4513e416ebba2d51c8eaa626c0f6b434d0d8
lucio <desouzaleitelucioweslley@gmail.com>
2025-04-19 23:57:19 -0300

    criando componente Input para ser usado em formulários

---
+++ frontend/src/components/Input.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default{
+    name:'Input'
+}
+</script>
+
+<template>
+    <input>
+</template>
+
+<style scoped>
+input{
+    font-size: 1.5rem;
+    width: 20vw;
+    height: 3vh;
+    border:none;
+    padding-left: 10px;
+    margin-bottom: 20px;
+    border-bottom: solid #041E31 1px;
+}
+
+input::placeholder{
+    color:#041E31;
+}
+</style>
