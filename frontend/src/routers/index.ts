ae95eb0706de7413af809986428d0a11f29375aa
lucio <desouzaleitelucioweslley@gmail.com>
2025-04-21 00:56:29 -0300

    adcionando proteção nas rotas para controle das páginas

--- frontend/src/routers/index.ts
+++ frontend/src/routers/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Login from "../views/Login.vue";
 import Cadastro from '../views/Cadastro.vue';
+import Home from '../views/Home.vue';
 
 const routes = [
     {
@@ -13,12 +14,24 @@ const routes = [
       path:"/cadastro",
       name:"cadastro",
       component:Cadastro
+    },
+    {
+      path:"/home",
+      name:"home",
+      component:Home
     }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  console.log(from.name);
+  const isAuth = localStorage.getItem('token')
+  if (to.name !== 'login' && !isAuth) next({ name: 'login' })
+  else next()
 })
 
 export default router
